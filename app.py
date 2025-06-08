b6a9797f237507692e690d979c2136770971a4dc
ravik009 <ravikumar619e@gmail.com>
2025-06-08 14:29:44 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from yt_dlp import YoutubeDL
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/download', methods=['POST'])
+def download():
+    data = request.get_json()
+    url = data.get('url')
+
+    if not url:
+        return jsonify({'error': 'No URL provided'}), 400
+
+    ydl_opts = {
+        'quiet': True,
+        'skip_download': True,
+        'forcejson': True
+    }
+
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            formats = [
+                {
+                    'format_id': f['format_id'],
+                    'ext': f['ext'],
+                    'format_note': f.get('format_note', ''),
+                    'resolution': f.get('resolution', ''),
+                    'filesize': f.get('filesize', 0),
+                    'url': f['url']
+                } for f in info['formats'] if f.get('url')
+            ]
+            return jsonify({
+                'title': info.get('title'),
+                'thumbnail': info.get('thumbnail'),
+                'formats': formats
+            })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
